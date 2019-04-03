f0c8f8185a136d6857f9cf6d77ff065c7d277ad5
Matthew Bishop <mattsacommonname@users.noreply.github.com>
2019-04-02 23:18:23 -0700

    Basic Flask skelleton.

--- main.py
+++ main.py
@@ -1,33 +1,19 @@
-from feedparser import FeedParserDict, parse
+from feedparser import parse
+from flask import Flask, render_template
 
 
+app = Flask(__name__)
 feed_uris = [
     'http://thisisindexed.com/feed/',
     'https://www.webtoons.com/en/comedy/behind-the-gifs/rss?title_no=658',
     'https://xkcd.com/atom.xml']
 
 
-def parse_entry(entry: FeedParserDict):
-    print(entry['title'])
+@app.route('/')
+def root():
+    feeds = []
+    for feed_uri in feed_uris:
+        feed = parse(feed_uri)
+        feeds.append(feed)
 
-
-def parse_feed(uri: str):
-    try:
-        feed = parse(uri)
-
-    except Exception as ex:
-        print(ex)
-        return
-
-    if feed is None or feed.bozo:
-        print(f'\n"{uri}" did not resolve\n')
-        return
-
-    print(f"\n{feed['feed']['title']}\n")
-
-    for entry in feed['entries']:
-        parse_entry(entry)
-
-
-for feed_uri in feed_uris:
-    parse_feed(feed_uri)
+    return render_template('root.html', feeds=feeds)
