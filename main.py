1a46b651b01461ecc8a0b5cde3d508be3b2d043e
Matthew Bishop <mattsacommonname@users.noreply.github.com>
2019-04-23 20:45:04 -0700

    Move installation specific data to subdirectory, and ignore it.

--- main.py
+++ main.py
@@ -57,7 +57,7 @@ class Entries(Resource):
 api.add_resource(Entries, '/entries')
 
 
-db.bind(provider='sqlite', filename='feeds.sqlite', create_db=True)
+db.bind(provider='sqlite', filename='data/feeds.sqlite', create_db=True)
 set_sql_debug(True)
 db.generate_mapping(create_tables=True)
 
